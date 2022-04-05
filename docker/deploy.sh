9fbda57e68590b624de5b15f4032ec09c0963572
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-04-05 11:31:15 -0400

    Use readable hostname on deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,14 +3,16 @@
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
-existing=$(docker ps -aq -f name=hydrogens)
+container_name=hydrogens
+existing=$(docker ps -aq -f name=$container_name)
 if [ -n "$existing" ]; then
     echo "removing existing container"
     docker rm -f $existing
 fi
 
 docker run -d \
---name hydrogens \
+--name $container_name \
 --restart unless-stopped \
+-h $(hostname)-$container_name \
 -e ARGS="$*" \
-hydrogens
+$container_name
